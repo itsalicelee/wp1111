c168596739cb521ea88d5da109353b500894d354
Alice <alice668149@gmail.com>
2022-09-30 00:48:38 +0800

    hw3: add line decoration if task is completed

--- hw3/src/components/Todo.js
+++ hw3/src/components/Todo.js
@@ -2,13 +2,23 @@ import React from 'react';
 import xIcon from '../img/x.png';
 
 function Todo(props) {
-    return props.tasks.map((task) => (
+    let tasks = props.tasks;
+    function handleCheck(event) {
+        let changedTask = tasks.find((task) => task.id.toString() === event.target.id);
+        changedTask.completed = !changedTask.completed;
+        // add or delete completed class for css styling
+        const h1 = event.target.parentNode.parentNode.childNodes[1];
+        if (changedTask.completed) h1.classList.add('todo-app__item-completed');
+        else h1.classList.remove('todo-app__item-completed');
+    }
+
+    return tasks.map((task) => (
         <li className="todo-app__item" key={task.id}>
             <div className="todo-app__checkbox">
-                <input type="checkbox" id={task.id} />
+                <input type="checkbox" id={task.id} onChange={(e) => handleCheck(e)} />
                 <label htmlFor={task.id} />
             </div>
-            <h1 className="todo-app__item-detail">{task.name}</h1>
+            <h1 className={'todo-app__item-detail'}>{task.name}</h1>
             <img alt="x-icon" src={xIcon} className="todo-app__item-x" />
         </li>
     ));
