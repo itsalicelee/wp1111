a3116d7838749c88055e289216ba4b098f94ccd2
Alice <alice668149@gmail.com>
2022-10-28 14:17:19 +0800

    hw5: add backend server

---
+++ hw5/backend/core/getNumber.js
@@ -0,0 +1,15 @@
+let number = 0;
+export const getNumber = () => {
+    return number;
+};
+
+export const genNumber = () => {
+    number = getRandomInt(0, 100);
+    return number;
+};
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
--- hw5/backend/routes/guess.js
+++ hw5/backend/routes/guess.js
@@ -1,13 +1,33 @@
-import express from 'express'
-const router = express.Router()
-router.post('/start', (_, res) => {
-genNumber() // 用亂數產生一個猜數字的 number，存在 memory DB res.json({ msg: 'The game has started.' })
-})
-router.get('/guess', (req, res) => {
+import express from "express";
+import { getNumber, genNumber } from "../core/getNumber";
+
+const router = express.Router();
+router.post("/start", (_, res) => {
+    genNumber(); // 用亂數產生一個猜數字的 number，存在 memory DB
+    res.json({ msg: "The game has started." });
+});
+
 // 去 (memory) DB 拿答案的數字
 // 用 req.query.number 拿到前端輸入的數字
 // check if NOT a num or not in range [1,100]
 // 如果有問題 =>
 // res.status(406).send({ msg: 'Not a legal number.' }) // 如果沒有問題，回傳 status
-router.post('/restart', (_, res) => { ... })
-export default router
+router.get("/guess", (req, res) => {
+    const ans = getNumber();
+    const guess = req.query.number;
+    if (!guess || guess < 1 || guess > 100) {
+        res.status(406).send({ msg: "Not a legal number." });
+    } else if (guess === ans) {
+        res.status(200).send("Equal");
+    } else if (guess < ans) {
+        res.status(200).send({ msg: "Bigger" });
+    } else if (guess > ans) {
+        res.status(200).send({ msg: "Smaller" });
+    }
+});
+
+router.post("/restart", (_, res) => {
+    const ans = genNumber();
+    res.json({ msg: `New game! The answer is ${ans}` });
+});
+export default router;
