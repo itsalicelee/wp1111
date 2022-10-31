1a51a8190447ebc7f5f33ee7ea01ced94c32411d
Alice <alice668149@gmail.com>
2022-10-31 12:50:44 +0800

    hw6: initial commit

---
+++ hw6/frontend/src/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: `http://localhost:4000/`,
+});
+
+export default instance;
+
+// instance.get('/hi').then((data) => console.log(data));
