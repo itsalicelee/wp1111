96de130027fc7b2cf3c1914d6dc16afcc9f5c402
Alice <alice668149@gmail.com>
2022-11-12 00:37:32 +0800

    hw7: add backend server code

--- hw7/backend/src/server.js
+++ hw7/backend/src/server.js
@@ -1,10 +1,11 @@
 import http from 'http';
 import express from 'express';
-import dotenv from 'dotenv-defaults'
+import dotenv from 'dotenv-defaults';
 import mongoose from 'mongoose';
-import mongo from './mongo'
+import mongo from './mongo';
 import WebSocket from 'ws';
-import wsConnect from './wsConnect'
+import wsConnect from './wsConnect';
+import { v4 as uuidv4 } from 'uuid';
 
 mongo.connect();
 
@@ -12,20 +13,18 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-
 const db = mongoose.connection;
-db.once('open', () => { 
-	console.log("MongoDB connected!"); 
-	wss.on('connection', (ws) => {
-		// ws is Client-side web socket object
-		
-		// wsConnect.initData(ws);
-		// ws.onmessage = wsConnect.onMessage(ws);
-		ws.onmessage = wsConnect.onMessage(wss, ws);
-
-	});
+db.once('open', () => {
+    console.log('MongoDB connected!');
+    wss.on('connection', (ws) => {
+        ws.id = uuidv4();
+        ws.box = ''; // keep track of client's CURRENT chat box
+        // wsConnect.initData(ws);
+        // ws is Client-side web socket object
+        ws.onmessage = wsConnect.onMessage(wss, ws);
+    });
 });
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => { 
-	console.log(`Listening on http://localhost:${PORT}`);
- });
+server.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
