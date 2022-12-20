55888344bb20ca966e7604423b7fbbcbff03589d
Alice <alice668149@gmail.com>
2022-12-20 10:43:20 +0800

    hack3

--- hack3/backend/src/resolvers/Mutation.js
+++ hack3/backend/src/resolvers/Mutation.js
@@ -1,45 +1,63 @@
 const Mutation = {
-  createItem: async (parent, { input }, {itemModel, pubSub}) => {
-    const newItem = new itemModel(input);
-    await newItem.save();
-    pubSub.publish("ITEM_CREATED", {
-      itemCreated: newItem,
-    });
-    return newItem;
-  },
+    createItem: async (parent, { input }, { itemModel, pubSub }) => {
+        const newItem = new itemModel(input);
+        await newItem.save();
+        pubSub.publish('ITEM_CREATED', {
+            itemCreated: newItem,
+        });
+        return newItem;
+    },
 
-  updateItem: async (parent, { input }, {itemModel, pubSub}) => {
-    const item = await itemModel.findOneAndUpdate(
-      { id: input.id },
-      {
-        $set: {
-          name: input.name,
-          amount: input.amount,
-          category: input.category,
-          date: input.date,
-          description: input.description,
-        },
-      }
-    );
-    const newItem = {
-      id: input.id ?? item.id,
-      name: input.name ?? item.name,
-      amount: input.amount ?? item.amount,
-      category: input.category ?? item.category,
-      date: input.date ?? item.date,
-      description: input.description ?? item.description,
-    }
-    pubSub.publish("ITEM_UPDATED", {
-      itemUpdated: newItem,
-    });
-    return newItem;
-  },
-  // TODO 5.2 Define the itemDelete mutation resolver
-  // TODO 6.3 Publish itemDeleted
+    updateItem: async (parent, { input }, { itemModel, pubSub }) => {
+        const item = await itemModel.findOneAndUpdate(
+            { id: input.id },
+            {
+                $set: {
+                    name: input.name,
+                    amount: input.amount,
+                    category: input.category,
+                    date: input.date,
+                    description: input.description,
+                },
+            }
+        );
+        const newItem = {
+            id: input.id ?? item.id,
+            name: input.name ?? item.name,
+            amount: input.amount ?? item.amount,
+            category: input.category ?? item.category,
+            date: input.date ?? item.date,
+            description: input.description ?? item.description,
+        };
+        pubSub.publish('ITEM_UPDATED', {
+            itemUpdated: newItem,
+        });
+        return newItem;
+    },
+    // TODO 5.2 Define the itemDelete mutation resolver
+    // TODO 6.3 Publish itemDeleted
+    deleteItem: async (parent, { id }, { itemModel, pubSub }) => {
+        console.log(id);
+        const deletedItem = await itemModel.findOne({ id: id });
+        console.log(deletedItem);
+        itemModel.remove(deletedItem);
 
-  // TODO 5.2 End
-  // TODO 6.3 End
+        //  db.comments = db.comments.filter((comment) => comment.post !== args.id);
 
+        //  if (item.published) {
+        //      pubsub.publish('post', {
+        //          post: {
+        //              mutation: 'DELETED',
+        //              data: post,
+        //          },
+        //      });
+        //  }
+
+        return id;
+    },
+
+    // TODO 5.2 End
+    // TODO 6.3 End
 };
 
 export default Mutation;
