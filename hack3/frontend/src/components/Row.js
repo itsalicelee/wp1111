79e875cb9fb0fcab59b6ef1a147b882563533c52
Alice <alice668149@gmail.com>
2022-12-20 10:17:06 +0800

    hack3

--- hack3/frontend/src/components/Row.js
+++ hack3/frontend/src/components/Row.js
@@ -43,7 +43,14 @@ function Row({
     updateItem({
       variables: {
         // TODO 4 Use `updateItem` and pass the correct variables
-
+        input: {
+          id: formData.id,
+          name: formData.name,
+          amount: formData.amount,
+          category: formData.category,
+          date: formData.date,
+          description: formData.description
+        },
         // TODO End
         onError: (err) => {
           // eslint-disable-next-line no-console
