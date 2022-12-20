3e2871d2cd6c6b64dff3415b9cf8359a176720fb
Alice <alice668149@gmail.com>
2022-12-20 09:40:20 +0800

    hack3

--- hack3/frontend/src/components/Analytics.js
+++ hack3/frontend/src/components/Analytics.js
@@ -7,7 +7,10 @@ import Category from './Category';
 
 function Analytics() {
   // TODO 2.2 Use the useQuery hook to get items from backend
-
+  const {
+    loading, error, data: itemsData, subscribeToMore,
+  } = useQuery(GET_ITEMS_QUERY);
+  const { items } = itemsData;
   // TODO 2.2 End
 
 
@@ -16,12 +19,12 @@ function Analytics() {
       { 
         // TODO 2.3 Add Balence and Category (uncomment the following code)
       }
-      {/* <div className="col-span-6">
+      <div className="col-span-6">
         <Balance items={items} />
       </div>
       <div className="col-span-6">
         <Category items={items} />
-      </div> */}
+      </div>
       {
         // TODO 2.3 End
       }
